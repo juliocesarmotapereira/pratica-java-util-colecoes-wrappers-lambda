f4a43b6e850eda5b392b3d7d23b490a1f432c629
Julio Mota <juliocesarmotapereira@gmail.com>
2023-02-12 16:15:36 -0300

    iniciando - ArrayList e Generics

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Material de estudos práticos do Programa ONE da Oracle
 * Array []:  são objetos, primeira posição indicada pelo index 0, inicializado com os valores padrões, é uma estrutura de dados.
 * NullPointerException: acessando um elemento do array quando a posição está null.
 * ArrayIndexOutOfBoundException: acessar uma posição inválida recebemos estáa exceção.
+* Alterar a mesma palavra em varios lugares: Refactor > Rename.
+* Type Cast: conversão de um tipo para outro.
+* Cast explícito e implícito.
+* ClassCastException: é um unchecked pois estende RuntimeException.
+* 
--- eclipse-java-util-colecoes-wrappers-lambda/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
+++ eclipse-java-util-colecoes-wrappers-lambda/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayReferencias.java
@@ -1,25 +1,30 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TesteArrayReferencias {
 
 	public static void main(String[] args) {
 
 		// int[] idades = new int[5];
-
-		ContaCorrente[] contas = new ContaCorrente[5];
+		Object[] referencias = new Object[5];
 
 		ContaCorrente cc1 = new ContaCorrente(222, 555);
-		contas[0] = cc1;
+		referencias[0] = cc1;
 
-		ContaCorrente cc2 = new ContaCorrente(222, 666);
-		contas[1] = cc2;
+		ContaPoupanca cc2 = new ContaPoupanca(222, 666);
+		referencias[1] = cc2;
 
-		// impressões iguais do array referência:
-		System.out.println(contas[1].getNumero());
+		Cliente cliente = new Cliente();
+		referencias[2] = cliente;
 
-		ContaCorrente ref = contas[1];
+		// Object referenciaGenerica = conta[1];
+		// System.out.println(contas[1].getNumero());
+
+		ContaPoupanca ref = (ContaPoupanca) referencias[1]; // type cast
 		System.out.println(ref.getNumero());
 
 		System.out.println(cc2.getNumero());
